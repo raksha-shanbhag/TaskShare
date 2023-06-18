417e5e5b1127cb257e8c7c255ceda7744af2f1af
LamiaUddin <lamiahameed@yahoo.com>
2023-06-17 20:10:00 -0400

    made basic create group screen and updated bottom bar styling

--- app/src/main/java/com/example/greetingcard/screens/CreateGroup.kt
+++ app/src/main/java/com/example/greetingcard/screens/CreateGroup.kt
@@ -1,40 +1,93 @@
 package com.example.greetingcard.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greetingcard.R
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun CreateGroupScreen(onBack: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
-        contentAlignment = Alignment.Center
-    ) {
-        Column() {
-            Text(
-                text = "create group",
-                fontSize = MaterialTheme.typography.h3.fontSize,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-
-            Button(onClick = onBack) {
-                Text("back")
-            }
-        }
-
+    var groupName by remember {
+        mutableStateOf("")
     }
+    var groupDescription by remember {
+        mutableStateOf("")
+    }
+    var member by remember {
+        mutableStateOf("")
+    }
+    var groupMembers by remember {
+        mutableStateOf(listOf<String>())
+    }
+
+    Scaffold( topBar = {TopAppBar(backgroundColor = colorResource(id = R.color.primary_blue), title = {Text(text="Create Group")}, contentColor = Color.White, navigationIcon = {
+        IconButton(
+        onClick = onBack ) {
+            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+        }
+    })}, content = {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Column (verticalArrangement = Arrangement.spacedBy(10.dp)) {
+
+                TextField(value = groupName, onValueChange = {text -> groupName = text},  label = {Text("Group Name")})
+
+                TextField(value = groupDescription, onValueChange = {text -> groupDescription = text}, label = {Text("Group Description")})
+
+                TextField(value = member, onValueChange = {text -> member = text}, label = {Text("Add Members")})
+
+                LazyColumn() {
+                    items(groupMembers) { currentGroup ->
+                        Text(text = currentGroup)
+                    }
+                }
+
+                Button(onClick = {
+                    groupMembers = groupMembers + member
+                    member = ""
+                }) {
+                    Text("add group member")
+                }
+
+            }
+
+        }
+    })
+
 }
 
