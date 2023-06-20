beb81fb3f2b788d40318fd92c92075c6a911de1e
Jaishree Bala <jaishree1bala@gmail.com>
2023-06-20 02:29:10 -0400

    Added barebones info for task page and styled the task card

--- TimeLog_Jaishree.md
+++ TimeLog_Jaishree.md
@@ -1,5 +1,10 @@
-| Date         | Jaishree   | Task                                                 |
-|--------------|------------|------------------------------------------------------|
-| 2023/06/14   | 0.5        | Redid and finalized designs to allow for a smooth UI |
-| 2023/06/15   | 3          | Redid and finalized designs to allow for a smooth UI |
-| ------------ | ---------- |------------------------------------------------------|
+| Date       | Jaishree | Task                                                                      |
+|------------|----------|---------------------------------------------------------------------------|
+| 2023/06/14 | 0.5      | Redid and finalized designs to allow for a smooth UI                      |
+| 2023/06/15 | 3        | Redid and finalized designs to allow for a smooth UI                      |
+| 2023/06/18 | 1        | Learning Jetpack Compose and planning how to implement UI for tasks pages |
+| 2023/06/19 | 0.5      | Learning Jetpack Compose and planning how to implement UI for tasks pages |
+| 2023/06/19 | 1        | setting up task page with barebones information                           |
+| 2023/06/19 | 2        | styling task card                                                         |
+| 2023/06/19 | 1        | dynamic task progress color and text update                               |
+
--- app/src/main/java/com/example/greetingcard/screens/MyTasks.kt
+++ app/src/main/java/com/example/greetingcard/screens/MyTasks.kt
@@ -1,18 +1,63 @@
 package com.example.greetingcard.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greetingcard.R
+
+var min_width_pill = 100
+var max_width_pill = 400
+@Composable
+fun RenderStatus(status: String){
+
+    var text_status = ""
+    var text_color = R.color.white
+
+    if(status == "todo"){
+        text_status = "To Do"
+        text_color = R.color.progress_red
+    }
+    if(status == "inprogress"){
+        text_status = "In Progress"
+        text_color = R.color.progress_yellow
+    }
+    if(status == "done"){
+        text_status = "Done"
+        text_color = R.color.progress_green
+    }
+
+    Text(text = text_status,
+        color = colorResource(id = R.color.white),
+
+        modifier = Modifier
+            .widthIn(min_width_pill.dp, max_width_pill.dp)
+            .background(colorResource(id = text_color), RoundedCornerShape(3.dp))
+            .padding(10.dp, 2.dp)
+    )
+}
 
 @Composable
 fun MyTasksScreen(showDetail: () -> Unit) {
@@ -23,19 +68,79 @@ fun MyTasksScreen(showDetail: () -> Unit) {
         contentAlignment = Alignment.Center
     ) {
         Column() {
-            Text(
-                text = "My Tasks",
-                fontSize = MaterialTheme.typography.h3.fontSize,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-
-            Button(onClick = showDetail) {
-                Text("view task")
+            Row(modifier) {
+                Text(
+                    text = "Tasks Remaining: "
+                )
+                Text(
+                    text = "0"
+                )
             }
+            Row(){
+                Text(text = "Sort By: ")
+                Text(text = "Groups")
+            }
+            Row(
+            modifier = Modifier
+                .background(colorResource(id = R.color.icon_blue), RoundedCornerShape(10.dp))
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+                ) {
+
+
+                Icon(
+                    painterResource(id = R.drawable.baseline_groups_24),
+                    contentDescription = "Default Group Icon",
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.white), RoundedCornerShape(10.dp))
+                        .padding(14.dp)
+                )
+
+                Column(modifier = Modifier
+                    .padding(10.dp, 0.dp))
+                {
+                    Text(text = "Clean counters",
+                        modifier = Modifier
+                            .padding(2.dp, 0.dp, 0.dp, 5.dp))
+                    Text(text = "Roommates",
+                        color = colorResource(id = R.color.dark_blue),
+                        modifier = Modifier
+                            .background(colorResource(id = R.color.white), RoundedCornerShape(3.dp))
+                            .padding(10.dp, 2.dp)
+                    )
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Center,
+
+                ) {
+                    RenderStatus("todo")
+                    RenderStatus("done")
+                    RenderStatus("inprogress")
+                    Spacer(modifier = Modifier.height(height = 5.dp))
+                    Text(text = "10/05/2023",
+                    modifier = Modifier
+                        .widthIn(min_width_pill.dp, max_width_pill.dp)
+                        .background(colorResource(id = R.color.white), RoundedCornerShape(3.dp))
+                        .padding(10.dp, 2.dp)
+                    )
+                }
+
+
+//            Text(
+//                text = "My Tasks",
+//                fontSize = MaterialTheme.typography.h3.fontSize,
+//                fontWeight = FontWeight.Bold,
+//                color = Color.Black
+//            )
+
+//            Button(onClick = showDetail) {
+//                Text("my x task")
+//            }
 
         }
 
     }
-}
+}}
 
