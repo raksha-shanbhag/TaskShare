5103db745acafbd3793b8d17f862e4e8574e52d9
LamiaUddin <lamiahameed@yahoo.com>
2023-06-22 04:27:13 -0400

    styled group tasks screen

--- app/src/main/java/com/TaskShare/UI/Views/screens/ViewGroup.kt
+++ app/src/main/java/com/TaskShare/UI/Views/screens/ViewGroup.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
