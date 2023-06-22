51d063e62924dffa1af5345f5c2d66f01eafa84b
Sheng <shengfang798@gmail.com>
2023-06-22 14:06:45 -0400

    Added log-in and sign-up screens

--- app/src/main/java/com/example/greetingcard/screens/Login.kt
+++ app/src/main/java/com/example/greetingcard/screens/Login.kt
@@ -1,33 +1,34 @@
 package com.example.greetingcard.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greetingcard.R
 
 @Composable
 fun LoginScreen() {
-    Box(
+    val primaryBlue = colorResource(id = R.color.primary_blue)
+    val backgroundBlue = colorResource(id = R.color.background_blue)
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
-        contentAlignment = Alignment.Center
+            //.background(Color.White)
+            .verticalScroll(rememberScrollState())
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "LOGIN",
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
+
     }
 }
+
 
 @Composable
 @Preview
