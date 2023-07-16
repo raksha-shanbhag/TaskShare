3c874ded118319a05008f5331f5aca82665eeea1
LamiaUddin <lamiahameed@yahoo.com>
2023-07-16 19:25:48 -0400

    adding checkbox

--- app/src/main/java/com/TaskShare/UI/Views/screens/CreateGroup.kt
+++ app/src/main/java/com/TaskShare/UI/Views/screens/CreateGroup.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -110,6 +111,8 @@ fun CreateGroupScreen(onBack: () -> Unit, viewModel: GroupViewModel) {
                             if(friends[i].isSelected) {
                                 Icon(imageVector = Icons.Default.Check,
                                     contentDescription = "selected" )
+                            } else {
+                                Icon(painterResource(id =  R.drawable.ic_sqaure), "square")
                             }
                         }
 
