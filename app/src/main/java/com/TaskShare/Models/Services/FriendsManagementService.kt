94ae8dfb8edd05471e4ea1175a73da2fc26f5018
Nour Elgawish <nourelgawish2001@gmail.com>
2023-07-18 10:19:46 -0400

    fixed and need fixing

--- app/src/main/java/com/TaskShare/Models/Services/FriendsManagementService.kt
+++ app/src/main/java/com/TaskShare/Models/Services/FriendsManagementService.kt
@@ -10,36 +10,58 @@ class FriendsManagementService {
     // API service for sending a friend request
     // params:- sender, receiverEmail
     // return:-
-    fun sendFriendRequest(senderId: String, receiverEmail: String){
-        if (senderId.isEmpty()) {
-            //need to know what to return
-        }
+    fun sendFriendRequest(currentUserId: String, receiverEmail: String){
+
+        val sender = TSUser.getFromId(currentUserId)
         var receiverId = usersRepository.getUserIdFromEmail(receiverEmail)
+        val receiver = TSUser.getFromId(receiverId)
+
+        if (currentUserId.isNotEmpty()) {
+            val senderNewFriend =  TSUser.Friend(userId = receiverId, name = receiver.userData.firstName, status = "Outgoing")
+            sender.userData.friends.add(senderNewFriend)
+        }
+
         if (receiverId.isNotEmpty()) {
-            val receiver = TSUser.getFromId(receiverId)
-            val newFriend =  TSUser.Friend(userId = receiverId, name = receiver.userData.firstName, status = "Pending")
+            val newFriend =  TSUser.Friend(userId = currentUserId, name = sender.userData.firstName, status = "Incoming")
             receiver.userData.friends.add(newFriend)
         }
     }
 
-    fun acceptFriendRequest(currentUserId: String, friendToAccept: String){
-        val sender = TSUser.getFromId(currentUserId)
-        val senderNewFriend =  TSUser.Friend(userId = currentUserId, name = sender.userData.firstName, status = "Friend")
-        sender.userData.friends.add(senderNewFriend)
-        //lines 30 - 31 are from ChatGpt
-        var currentUser = TSUser.getFromId(currentUserId)
-        var friendRequest = currentUser.userData.friends.find { friend -> friend.userId == friendToAccept }
-        friendRequest?.let { friend ->
-            friend.status = "Friend"
-        }
+    fun acceptFriendRequest(currentUserId: String, friendToAcceptId: String){
 
+        val acceptor = TSUser.getFromId(currentUserId)
+        var friendToAccept = TSUser.getFromId(friendToAcceptId)
+
+        if (currentUserId.isNotEmpty()) {
+            var incomingToFriend = acceptor.userData.friends.find { friend -> friend.userId == friendToAcceptId }
+            incomingToFriend?.let { friend ->
+                friend.status = "Friend"
+            }
+        }
+        if (friendToAcceptId.isNotEmpty()) {
+            var outgoingToFriend = friendToAccept.userData.friends.find{friend -> friend.userId == currentUserId}
+            outgoingToFriend?.let { friend ->
+                friend.status = "Friend"
+            }
+
+        }
     }
 
-    fun declineFriendRequest(currentUserId: String, friendToDecline: String){
-        var currentUser = TSUser.getFromId(currentUserId)
-        var friendRequest = currentUser.userData.friends.find { friend -> friend.userId == friendToDecline }
-        friendRequest?.let { friend ->
-            friend.status = "Not A Friend"
+    fun declineFriendRequest(currentUserId: String, friendToDeclineId: String){
+        val decliner = TSUser.getFromId(currentUserId)
+        var friendToDecline = TSUser.getFromId(friendToDeclineId)
+
+        if (currentUserId.isNotEmpty()) {
+            var incomingToDeclined = decliner.userData.friends.find { friend -> friend.userId == friendToDeclineId }
+            incomingToDeclined?.let { friend ->
+                friend.status = "Declined" //delete entery
+            }
+        }
+        if (friendToDeclineId.isNotEmpty()) {
+            var outgoingToRejected= friendToDecline.userData.friends.find{friend -> friend.userId == currentUserId}
+            outgoingToRejected?.let { friend ->
+                friend.status = "Rejected"//delete entery
+            }
         }
     }
 
@@ -68,4 +90,18 @@ class FriendsManagementService {
         }
 
     }
+    fun showMyFriends(){
+
+    }
+
+    fun showMyBlockedFriends(){
+
+    }
+    fun showIncomingFriendRequests(){
+
+    }
+    fun showOutgoingFriendRequests(){
+
+    }
+
 }
